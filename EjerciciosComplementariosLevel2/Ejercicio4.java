abaa97be03e41cd3f8926be30ac6bc6e8496cee3
SebastianAlejandro7 <54823741+SebastianAlejandro7@users.noreply.github.com>
2021-10-14 22:38:00 -0300

    Add files via upload

---
+++ EjerciciosComplementariosLevel2/Ejercicio4.java
@@ -0,0 +1,30 @@
+public class DemoArray {
+    public static void main(String args[])
+    {
+        int lista[]= new int ;
+        int num[]={1,2,3};
+        int tabla[][]={
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };
+
+        System.out.println("Longitud de lista: "+lista.length);
+        System.out.println("Longitud de num: " +num.length);
+        System.out.println("Longitud de tabla: "+tabla.length);
+        System.out.println("Longitud de tabla: " +tabla.length);
+        System.out.println("Longitud de tabla: " +tabla.length);
+        System.out.println("Longitud de tabla: " +tabla.length);
+
+        for (int i=0; i < lista.length; i++)
+            lista=i*i;
+
+        System.out.print("La lista es: ");
+
+        for (int i=0; i < lista.length; i++)
+            System.out.print(lista+ " ");
+        System.out.println();
+
+
+    }
+}
---
+++ EjerciciosComplementariosLevel2/Ejercicio5.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Empleado {
+    public static void main(String[] args) {
+
+        double sueldohora = 0;
+        int horasextras = 0;
+        double sueldohe = 0;
+        double impuestos = 0;
+        int hrstrabajadas = 0;
+        double sueldo = 0;
+        double sueldofinal = 0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el sueldo del empleado x hora");
+        sueldohora = entrada.nextDouble();
+
+        System.out.println("Ingrese las horas trabajadas a la semana");
+        hrstrabajadas = entrada.nextInt();
+
+        if (hrstrabajadas > 40) {
+            horasextras = hrstrabajadas - 40;
+        }
+        sueldohe = horasextras * sueldohora * 2;
+
+        sueldo = sueldohora * (hrstrabajadas + horasextras);
+
+        if (sueldo >= 20000) {
+            impuestos = sueldo * 0.25;
+        } else {
+            if (sueldo >= 15000) {
+                impuestos = sueldo * 0.20;
+            } else {
+                if (sueldo >= 10000) {
+                    impuestos = sueldo * 0.10;
+                } else {
+                    impuestos = 0;
+                }
+            }
+        }
+        sueldofinal = sueldo - impuestos;
+        System.out.println("El Sueldo del Empleado es: " + sueldofinal);
+    }
+}
