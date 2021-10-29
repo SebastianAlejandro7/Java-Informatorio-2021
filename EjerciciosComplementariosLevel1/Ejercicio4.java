014886627bcbbaf7da806e372e08a59599a236b7
SebastianAlejandro7 <54823741+SebastianAlejandro7@users.noreply.github.com>
2021-10-29 20:41:03 -0300

    Update Ejercicio4.java

--- EjerciciosComplementariosLevel1/Ejercicio4.java
+++ EjerciciosComplementariosLevel1/Ejercicio4.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
-
 public class Ejercicio4 {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Introduzca el numero del que desee obtener el factorial:");
-        int num = scan.nextInt();
-        scan.close();
-        int factorial = obtenerFactorial(num);
-        System.out.println("El factorial de " + num + " es: " + factorial);
-    }
 
-    public static int obtenerFactorial(int numero) {
-        int factorial = 1;
-        for (int n=1;n<=numero;n++){
-            factorial = factorial * n;
-        }
-        return factorial;
+    public double factorial(int n){
+        if (n==0)
+            return 1;
+        else
+            return n*(factorial(n-1));
     }
 }
