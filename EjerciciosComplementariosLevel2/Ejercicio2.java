99ae0af35e56afd9343d4e9a1ea7d7a5071c598e
SebastianAlejandro7 <54823741+SebastianAlejandro7@users.noreply.github.com>
2021-10-29 22:51:42 -0300

    Update Ejercicio2.java

--- EjerciciosComplementariosLevel2/Ejercicio2.java
+++ EjerciciosComplementariosLevel2/Ejercicio2.java
@@ -8,14 +8,12 @@ public class Ejercicio2 {
         numeros.add(23);
         numeros.add(90);
         numeros.add(8);
-
         System.out.println("===========================");
         System.out.println("Lista antes de los cambios:");
         System.out.println("===========================");
         iterarEImprimirLista(numeros);
         agregarNumeros(numeros);
     }
-
     static void agregarNumeros(ArrayList<Integer> list) {
         list.add(0, 4321);
         list.add(1234);
@@ -24,7 +22,6 @@ public class Ejercicio2 {
         System.out.println("=============================");
         iterarEImprimirLista(list);
     }
-
     static void iterarEImprimirLista(ArrayList<Integer> list) {
         int tamanio = 0;
         System.out.println("Elementos de la lista:");
