5cad378e6916b90f602cd08e8b35490643baf8dd
SebastianAlejandro7 <54823741+SebastianAlejandro7@users.noreply.github.com>
2021-10-29 22:58:03 -0300

    Update Ejercicio6.java

--- EjerciciosComplementariosLevel1/Ejercicio6.java
+++ EjerciciosComplementariosLevel1/Ejercicio6.java
@@ -12,17 +12,14 @@ public class Ejercicio6 {
         int resultado = potencia(num1, num2);
         System.out.println(num1 + " elevado a " + num2 + " = " + resultado);
     }
-
     static int potencia(int num1, int num2) {
         int resultado = 1;
 
         for (int i=0; i<num2; i++) {
             resultado = multiplicacion(resultado, num1);
         }
-
         return resultado;
-    }
-
+        }
     static int multiplicacion(int num1, int num2) {
         int resultado = 0;
         for (int i=0; i<num2; i++) {
