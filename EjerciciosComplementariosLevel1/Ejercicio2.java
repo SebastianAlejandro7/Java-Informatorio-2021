0afa9f590916d6f6933cfd00cbe706bfeb820c10
SebastianAlejandro7 <54823741+SebastianAlejandro7@users.noreply.github.com>
2021-10-29 22:56:38 -0300

    Update Ejercicio2.java

--- EjerciciosComplementariosLevel1/Ejercicio2.java
+++ EjerciciosComplementariosLevel1/Ejercicio2.java
@@ -8,16 +8,13 @@ public class Ejercicio2 {
         int num1 = scan.nextInt();
         System.out.println("Introduzca otro numero:");
         int num2 = scan.nextInt();
-
         int resultadoSuma = num1 + num2;
         int resultadoResta = num1-num2;
-
         System.out.println(num1 + " + " + num2 + " = " + resultadoSuma);
         System.out.println(num1 + " - " + num2 + " = " + resultadoResta);
         System.out.println(num1 + " * " + num2 + " = " + num1 * num2);
         System.out.println(num1 + " / " + num2 + " = " + num1 / num2);
         System.out.println(num1 + " % " + num2 + " = " + num1 % num2);
-
         scan.close();
     }
 }
